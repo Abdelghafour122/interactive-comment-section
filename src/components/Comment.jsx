441f37b7f5a01c80a8491a9714ddf526bf02ce1a
Abdelghafour <ghafourragg@gmail.com>
2022-03-31 16:30:34 +0100

    made the styles file

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,17 @@
 import React, { useContext } from "react";
-import { Avatar, Button, Card, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Card,
+  Stack,
+  Typography,
+  ThemeProvider,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import { Reply } from "@mui/icons-material/";
 import CommentContext from "../commentContext";
 import ScoreChanger from "./ScoreChanger";
+import theme from "../styles";
 
 const Comment = ({ onPass }) => {
   const { content, createdAt, score, replies, user } = onPass;
@@ -11,37 +19,52 @@ const Comment = ({ onPass }) => {
   const userName = user.username;
   const ava = IMGOBJ[`${userName}`];
   return (
-    <Card>
-      <Box sx={{ p: "15px" }}>
-        <Stack spacing={2} direction="row">
-          <Box>
-            <ScoreChanger />
-          </Box>
-          <Box>
-            <Stack
-              spacing={2}
-              direction="row"
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Stack spacing={2} direction="row" alignItems="center">
-                <Avatar src={ava}></Avatar>
-                <Typography fontWeight="bold"> {userName} </Typography>
-                <Typography> {createdAt} </Typography>
-              </Stack>
-              <Button
-                variant="text"
-                sx={{ fontWeight: "bold" }}
-                startIcon={<Reply />}
+    <ThemeProvider theme={theme}>
+      <Card>
+        <Box sx={{ p: "15px" }}>
+          <Stack spacing={2} direction="row">
+            <Box>
+              <ScoreChanger />
+            </Box>
+            <Box>
+              <Stack
+                spacing={2}
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
               >
-                Reply
-              </Button>
-            </Stack>
-            <Typography> {content} </Typography>
-          </Box>
-        </Stack>
-      </Box>
-    </Card>
+                <Stack spacing={2} direction="row" alignItems="center">
+                  <Avatar src={ava}></Avatar>
+                  <Typography
+                    fontWeight="bold"
+                    sx={{ color: "neutral.darkBlue" }}
+                  >
+                    {userName}
+                  </Typography>
+                  <Typography sx={{ color: "neutral.grayishBlue" }}>
+                    {createdAt}
+                  </Typography>
+                </Stack>
+                <Button
+                  variant="text"
+                  sx={{
+                    fontWeight: 500,
+                    textTransform: "normal",
+                    color: "custom.moderateBlue",
+                  }}
+                  startIcon={<Reply />}
+                >
+                  Reply
+                </Button>
+              </Stack>
+              <Typography sx={{ color: "neutral.grayishBlue" }}>
+                {content}
+              </Typography>
+            </Box>
+          </Stack>
+        </Box>
+      </Card>
+    </ThemeProvider>
   );
 };
 export default Comment;
---
+++ src/styles.js
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    custom: {
+      moderateBlue: "hsl(238, 40%, 52%)",
+      softBlue: "hsl(358, 79%, 66%)",
+      lightGrayishBlue: "hsl(239, 57%, 85%)",
+      paleRed: "hsl(357, 100%, 86%)",
+    },
+    neutral: {
+      darkBlue: "hsl(212, 24%, 26%)",
+      grayishBlue: "hsl(211, 10%, 45%)",
+      lightGray: "hsl(223, 19%, 93%)",
+      veryLightGray: "hsl(228, 33%, 97%)",
+      white: "#FFF",
+    },
+  },
+  typography: {
+    fontSize: 16,
+    fontFamily: "Rubik",
+  },
+});
+
+export default theme;
